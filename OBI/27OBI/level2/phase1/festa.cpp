3f4f7d42cdbd7a64898efd8b392a9d83206f5455
Pedro Luis Dias <luisp.diias@gmail.com>
2025-06-15 01:22:53 -0300

    Add implementation of Festa problem

---
+++ OBI/27OBI/level2/phase1/festa.cpp
@@ -0,0 +1,23 @@
+/*
+ * Festa
+ * Fase 1 — OBI2025 (Nivel 2)
+ * https://github.com/diaslui/competitive_programming
+*/
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int e, s, l;
+    cin >> e;
+    cin >> s;
+    cin >> l;
+
+    int maxVal = max({e,s,l});
+    int minVal = min({e,s,l});
+
+    cout << 2* (maxVal-minVal) << endl;
+
+    return 0;
+};
